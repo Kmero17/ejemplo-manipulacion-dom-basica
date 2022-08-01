b639e6df0f4489eeccd633f83903509496159e7c
Kmero <alejandrocamero01@gmail.com>
2022-08-01 16:24:54 -0400

    Agregar .gitignore y primeros archivos

---
+++ script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const input1 = document.getElementById("calculo1");
+const input2 = document.getElementById("calculo2");
+const button = document.getElementById("btnCalcular");
+const pResult = document.getElementById("result");
+/* form.addEventListener("submit", sumarInputValues);
+function sumarInputValues (event) {
+    //console.log ({event});
+    event.preventDefault();
+    const sumaInputs = (input1.value + " " + input2.value);
+    pResult.innerText= "Resultado: " + sumaInputs + " hackeaste la NASA"
+} */
